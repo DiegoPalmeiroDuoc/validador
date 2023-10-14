b1c4a7ad498bfe25fd5f1839270dd0890abd3277
DiegoPalmeiroSilva <diego@onesla.com>
2023-10-14 15:36:00 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'validador',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
